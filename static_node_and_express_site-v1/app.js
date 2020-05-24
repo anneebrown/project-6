5de4ef9afbf2bfc376292038dce948ede8027d23
anneebrown <61443022+anneebrown@users.noreply.github.com>
2020-05-23 19:27:01 -0700

    included a gitignore file

--- static_node_and_express_site-v1/app.js
+++ static_node_and_express_site-v1/app.js
@@ -1,6 +1,20 @@
 const express = require('express');
+const app= express();
 const path = require('path');
-const dataJSON = require('/data.json');
+const dataJSON = require('./data.json').projects;
 
 app.set('view engine', 'pug');
-app.use(express.static('public'))
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.locals = dataJSON;
+    res.render('index');
+})
+
+app.get('/about', (req, res) => {
+    res.render('about');
+})
+
+app.listen(3000, () => {
+    console.log('The application is running on localhost:3000');
+});
