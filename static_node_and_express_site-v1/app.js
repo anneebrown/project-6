d4793b044df5e47ca8e4608837985e1b1d7fdb0f
anneebrown <61443022+anneebrown@users.noreply.github.com>
2020-05-24 20:45:15 -0700

    mostly functional
    added error.pug
    links to projects work now and display content

--- static_node_and_express_site-v1/app.js
+++ static_node_and_express_site-v1/app.js
@@ -7,12 +7,26 @@ app.set('view engine', 'pug');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.locals = dataJSON;
+    res.locals.dataJSON = dataJSON;
+    console.log(dataJSON[0].technologies[1]);
     res.render('index');
 })
 
 app.get('/about', (req, res) => {
     res.render('about');
+})
+
+app.get('/:id', (req, res) => {
+    const { id } = req.params; 
+    res.locals.dataJSON = dataJSON[`${id}`];
+    console.log(dataJSON[0])
+    res.render('project');
+})
+
+app.use( (err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error', err);
 })
 
 app.listen(3000, () => {
