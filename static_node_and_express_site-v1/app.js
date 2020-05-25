ea799f0cfeb9070b4e08e4352e4743d5a387baa6
anneebrown <61443022+anneebrown@users.noreply.github.com>
2020-05-25 10:19:43 -0700

    error not working correctly

--- static_node_and_express_site-v1/app.js
+++ static_node_and_express_site-v1/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
 const dataJSON = require('./data.json').projects;
+var bodyParser = require('body-parser')
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: false }))
 app.set('view engine', 'pug');
 app.use(express.static('public'));
 
@@ -23,19 +25,23 @@ app.get('/:id', (req, res) => {
     res.locals.dataJSON = dataJSON[`${id}`];
     //console.log(dataJSON[0])
     res.render('project');
+    console.log(typeof id)
 })
 
-app.use((req, res, next) => {
+  app.use((req, res, next) => {
     const err = new Error('Not Found');
     err.status = 404;
     next(err);
 })
 
 app.use( (err, req, res, next) => {
-    res.locals.error = err;
-    res.status(err.status);
+    res.locals.err = err;
+    const status = err.status || 500;
+    res.status(status);
     res.render('error', err);
+    next();
 })
+
 
 app.listen(3000, () => {
     console.log('The application is running on localhost:3000');
